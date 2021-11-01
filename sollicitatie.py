bed29750e4479f2ad2248b2df39aaa8ca225747b
Piotrek <99058308@mydavinci.nl>
2021-11-01 09:40:31 +0100

    Git

---
+++ sollicitatie.py
@@ -0,0 +1,14 @@
+input('Wat is uw naam?')
+
+
+input ('')
+
+#Meer dan 4 jaar praktijkervaring met dieren-dressuur OF meer dan 5 jaar ervaring met jongleren OF meer dan 3 jaar praktijkervaring met acrobatiek
+#In bezit van een Diploma MBO-4 ondernemen
+#In bezit van een geldig Vrachtwagen rijbewijs
+#In bezit van een hoge hoed
+#Is man EN heeft Snor breder dan 10 cm OF is vrouw EN draagt rood krulhaar langer dan 20 cm.
+#Let op: Je hoeft alleen een man naar zijn snorlengte te vragen en alleen een vrouw naar haar krulhaarlengte.
+#Is langer dan 150 cm
+#Is zwaarder dan 90 kg
+#Heeft Certificaat “Overleven met gevaarlijk personeel”
