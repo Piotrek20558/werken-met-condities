8b82a65b87b627346067ec2035e81134fcb26759
Piotrek <99058308@mydavinci.nl>
2021-11-01 10:30:44 +0100

    Git

--- sollicitatie.py
+++ sollicitatie.py
@@ -1,8 +1,18 @@
-input('Wat is uw naam?')
+Vraag1 = input('Hoeveel jaar praktijkervaring heeft u met dieren-dressuur?')
+Vraag2 = input('Hoeveel jaar ervaring heeft u met jongleren?')
+Vraag3 = input('Hoeveel praktijkervaring heeft u met acrobatiek?')
+Vraag4 = input('Bent u in bezit van een Diploma MBO-4 ondernemen?')
+Vraag5 = input('Bent u in bezit van een geldig Vrachtwagen rijbewijs?')
+Vraag6 = input('Bent u in bezit van een hoge hoed?')
+Vraag7 = input('Bent u een man?')
+Vraag8 = input('Hoelang is uw snor?')
+Vraag9 = input('Bent u een vrouw?')
+Vraag10 = input('Hoelang is uw haar?')
+Vraag11 = input('Hoe zwaar bent u?')
+Vraag12 = input('Hoelang bent u?')
+Vraag13 = input('Heeft u Certificaat “Overleven met gevaarlijk personeel?')
 
-
-input ('')
-
+if Vraag1 <=4 or Vraag2 <=5 or Vraag3 <=3 or Vraag4 == "n" or Vraag
 #Meer dan 4 jaar praktijkervaring met dieren-dressuur OF meer dan 5 jaar ervaring met jongleren OF meer dan 3 jaar praktijkervaring met acrobatiek
 #In bezit van een Diploma MBO-4 ondernemen
 #In bezit van een geldig Vrachtwagen rijbewijs
