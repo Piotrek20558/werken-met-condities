b1be547ab179ee57a59d19ff8d8bbaf7e5f56855
Piotrek <99058308@mydavinci.nl>
2021-11-03 09:53:57 +0100

    Git

--- sollicitatie.py
+++ sollicitatie.py
@@ -12,13 +12,8 @@ Vraag11 = input('Hoe zwaar bent u?')
 Vraag12 = input('Hoelang bent u?')
 Vraag13 = input('Heeft u Certificaat “Overleven met gevaarlijk personeel?')
 
-if Vraag1 <=4 or Vraag2 <=5 or Vraag3 <=3 or Vraag4 == "n" or Vraag5 == "n" or Vraag6 == "no" or Vraag7 == "no"
-#Meer dan 4 jaar praktijkervaring met dieren-dressuur OF meer dan 5 jaar ervaring met jongleren OF meer dan 3 jaar praktijkervaring met acrobatiek
-#In bezit van een Diploma MBO-4 ondernemen
-#In bezit van een geldig Vrachtwagen rijbewijs
-#In bezit van een hoge hoed
-#Is man EN heeft Snor breder dan 10 cm OF is vrouw EN draagt rood krulhaar langer dan 20 cm.
-#Let op: Je hoeft alleen een man naar zijn snorlengte te vragen en alleen een vrouw naar haar krulhaarlengte.
-#Is langer dan 150 cm
-#Is zwaarder dan 90 kg
-#Heeft Certificaat “Overleven met gevaarlijk personeel”
+if Vraag1 <=4 or Vraag2 <=5 or Vraag3 <=3 or Vraag4 == "n" or Vraag5 == "n" or Vraag6 == "no" or Vraag7 == "no":
+ print('U komt in aanmerking voor een sollicitatiegesprek, stuur snel uw CV.')
+ 
+else:
+    print('U voldoet niet aan onze strenge eisen voor deze functie, het spijt ons.')
